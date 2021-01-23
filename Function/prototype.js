1736a1bd9dd0f7d825b7a8eb55bf177f8cd027a2
田湘龙 <727393818@qq.com>
2021-01-23 21:43:03 +0800

    原型链demo

--- Function/prototype.js
+++ Function/prototype.js
@@ -1,12 +1,13 @@
-const obj = {};
+const obj = {}
 function Person() {}
 
-const p = new Person();
+const p = new Person()
 
-p.__proto__ === Person.prototype;
-Person.prototype.constructor === Person;
+p.__proto__ === Person.prototype
+Person.prototype.constructor === Person
 
-Person.__proto__ === Object.prototype;
-Object.prototype === null;
+Person.__proto__ === Function.prototype
+Function.prototype.__proto__ === Object.prototype
+Object.prototype.__proto__ === null
 
-obj.__proto__ === Object.prototype;
+obj.__proto__ === Object.prototype
